3839a10359a37d5a68d6c8135db77ca422c4829f
velosiped-05 <velosiped_05@mail.ru>
2025-04-13 16:49:16 +0300

    Delete sync_to_github.ps1

--- sync_to_github.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "C:\KBinformer\webapp"
-git pull origin main --rebase
-if (git status --porcelain) {
-    git add .
-    git commit -m "Auto-sync $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-}
-git push origin main
