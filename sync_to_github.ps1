55b115690b071d5cde6191d22911822db4672319
velosiped-05 <velosiped_05@mail.ru>
2025-04-13 16:33:59 +0300

    Auto-sync 2025-04-13 16:33:59

--- sync_to_github.ps1
+++ sync_to_github.ps1
@@ -1,6 +1,7 @@
 Set-Location "C:\KBinformer\webapp"
+git pull origin main --rebase
 if (git status --porcelain) {
     git add .
     git commit -m "Auto-sync $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-    git push origin main
 }
+git push origin main
